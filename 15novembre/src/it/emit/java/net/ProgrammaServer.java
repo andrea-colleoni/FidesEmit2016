151878171fabf1a6f9fba3d785e7965571861ff8
Andrea Colleoni <andrea@string.software>
2016-11-15 12:54:47 +0100

    stanza chat

--- 15novembre/src/it/emit/java/net/ProgrammaServer.java
+++ 15novembre/src/it/emit/java/net/ProgrammaServer.java
@@ -1,26 +1,48 @@
 package it.emit.java.net;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProgrammaServer {
+	
+	class ClientConnesso {
+		Socket s;
+		PrintWriter pw;
+		String nome;
+		
+		public ClientConnesso(Socket s) {
+			super();
+			this.s = s;
+			try {
+				this.pw = new PrintWriter(
+						new OutputStreamWriter(s.getOutputStream()));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	List<ClientConnesso> clientConnessi;
 
 	public static void main(String[] args) {
 		ProgrammaServer prg = new ProgrammaServer();
 		prg.avvia();
 	}
-	
+
 	public void avvia() {
+		clientConnessi = new ArrayList<>();
 		try (ServerSocket ss = new ServerSocket(5000)) {
 			System.out.println("Server avviato sulla porta 5000");
-			while(true) {
+			while (true) {
 				Socket s = ss.accept();
+				clientConnessi.add(new ClientConnesso(s));
 				System.out.println("Nuova connessione da " + s.getRemoteSocketAddress().toString());
 				new Thread(() -> {
 					try {
@@ -37,18 +59,17 @@ public class ProgrammaServer {
 	}
 
 	public void echo(Socket s) throws IOException {
-		BufferedReader br = 
-				new BufferedReader(
-						new InputStreamReader(s.getInputStream()));
-		PrintWriter pw = 
-				new PrintWriter(
-						new OutputStreamWriter(s.getOutputStream()));		
+		BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+		String nome = br.readLine();
 		String messaggio = "";
-		while(!messaggio.equals("esci")) {
+		while (!messaggio.equals("esci")) {
 			messaggio = br.readLine();
 			System.out.println("console: " + messaggio);
-			pw.println("socket : " + messaggio);
-			pw.flush();
+			for (ClientConnesso cc : clientConnessi) {
+				cc.pw.println(nome + " : " + messaggio);
+				cc.pw.flush();
+			}
 		}
 		s.close();
 	}
