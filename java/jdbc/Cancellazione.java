1be528203433c92911f2dbe1f1a86bdecc06628b
Gianluca Cannata <gcannata23@gmail.com>
2016-12-04 17:11:13 +0100

    Aggiunta cartella java/jdbc con relativi programmi per interrogare il database
    Attualmente i programmi sono:
    Inserimento.java: inserisce un nuovo record
    Cancellazione.java: cancella un record esistente
    Nota: Questi due primi programmi mostrano esempi estremamente basici su come interrogare il database tramite le operazioni SQL di inserimento e cancellazione di record.

---
+++ java/jdbc/Cancellazione.java
@@ -0,0 +1,47 @@
+/**
+ * @author aculnaig
+ *
+ */
+import java.sql.*;
+
+public class Cancellazione {
+
+	public static void main(String[] args) {
+		
+		// Crea un URL per connettersi al database 'scuola'	
+		String url = "jdbc:mysql://127.0.0.1:3306/scuola";
+		Connection con;
+		String sql = "DELETE FROM studente WHERE matricola = 0";
+		Statement stmt;
+		int righe;
+		
+		// Connessiona al database
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch(ClassNotFoundException e) {
+			System.err.println("ClassNotFoundException: " + e.getMessage());
+		}
+		
+		try {
+			// getConnection(url, username, password)
+			// url: l'URL creato all'inizio del main
+                        // username: il nome utente che avete scelto durante l'installaizone di MySQL
+                        // password: la password che avete scelto durante l'installaizone di MySQL
+			con  = DriverManager.getConnection(url, "", "");
+			stmt = con.createStatement();
+
+			// Esegue il comando SQL
+			righe = stmt.executeUpdate(sql);
+			
+			if (righe > 0)
+				System.out.println("Record eliminato.");
+			
+			else 
+				System.out.println("Record da eliminare non trovato");
+		} catch (SQLException ex) {
+			System.err.println("SQLException: " + ex.getMessage());
+		}
+		
+	}
+
+}
---
+++ java/jdbc/Inserimento.java
@@ -0,0 +1,46 @@
+/**
+ * @author aculnaig
+ *
+ */
+
+import java.sql.*;
+
+public class Inserimento {
+
+	public static void main(String[] args) {
+
+		// Crea un URL per connettersi al database 'scuola'
+		String url = "jdbc:mysql://127.0.0.1:3306/scuola";
+		Connection con;
+		String sql = "INSERT INTO studente (matricola, nome, cognome) VALUES (0, 'Giuseppe', 'Verdi')";
+		Statement stmt;
+
+		// Connessione al database
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.err.println("ClassNotFoundException: " + e.getMessage());
+		}
+
+		try {
+		        // getConnection(url, username, password)
+		        // url: l'URL create all'inizio del main
+			// username: il nome utente che avete scelto durante l'installaizone di MySQL
+		        // password: la password che avete scelto durante l'installaizone di MySQL
+			con  = DriverManager.getConnection(url, "", "");
+			stmt = con.createStatement();
+
+			// Esegue il comando SQL
+			stmt.executeUpdate(sql);
+
+			System.out.println("Record registrato");
+
+			stmt.close();
+			con.close();
+		} catch (SQLException ex) {
+			System.out.println("SQLException: " + ex.getMessage());
+		}
+
+	}
+
+}
